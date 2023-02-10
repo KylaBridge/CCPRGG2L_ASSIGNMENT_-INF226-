fbaf8bdbf68c9b72f3985d05a4c83eef8f629392
Kyla Bridget L. Rosaldo <119913576+KylaBridge@users.noreply.github.com>
2023-02-10 14:22:11 +0800

    Add files via upload

---
+++ exercise5/Daddy.java
@@ -0,0 +1,11 @@
+public class Daddy {
+    
+   protected String surname = "Rosaldo";
+
+   //method to show surname
+   public void showSurname(){
+
+    System.out.println("My family name is " + surname);
+
+   }
+}
---
+++ exercise5/Person.java
@@ -0,0 +1,15 @@
+public class Person {
+    
+    private String name = "kyla";
+
+    // get method()
+    public String getName(){
+        return this.name;
+    }
+
+    // set method()
+    public void setName(String personName ){
+        this.name = personName;
+    }
+    
+}
